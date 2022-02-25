2cdb6fae9bebaf50cc0ec5e9d1357522e06d5f41
chuvoddolyhood <trannhannghia.24012000@gmail.com>
2022-02-25 11:47:45 +0700

    completed to sign in an email

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:westside_sneaker_store/pages/register_page.dart';
@@ -12,11 +14,79 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-//Get value from textfield
-final email_controller_login = TextEditingController();
-final password_controller_login = TextEditingController();
-
 class _LoginPageState extends State<LoginPage> {
+  //Build alert dialog to show error
+  Future<void> alertDialogBuider(String error) async {
+    return showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Error'),
+            content: Container(
+              child: Text(error),
+            ),
+            actions: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text('Close'))
+            ],
+          );
+        });
+  }
+
+  //default state loading button
+  bool showLoading = false;
+
+  //Get value from textfield
+  final email_controller_login = TextEditingController();
+  final password_controller_login = TextEditingController();
+
+  //Sign in account
+  Future<String?> signinAccount() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(
+              email: email_controller_login.text,
+              password: password_controller_login.text);
+      return null;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        return 'No user found for that email.';
+      } else if (e.code == 'wrong-password') {
+        return 'Wrong password provided for that user.';
+      }
+      return e.message;
+    } catch (e) {
+      return e.toString();
+    }
+  }
+
+  //submit Signin form
+  void submitForm() async {
+    //Set form loading state
+    setState(() {
+      showLoading = true;
+    });
+
+    //Sign in account
+    String? createFeedbackAccount = await signinAccount();
+
+    //If string isn't null => error
+    if (createFeedbackAccount != null) {
+      alertDialogBuider(createFeedbackAccount);
+
+      //Cancel show loading form
+      setState(() {
+        showLoading = false;
+      });
+    } else {
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,10 +119,12 @@ class _LoginPageState extends State<LoginPage> {
                 appButton(
                   label: 'Login',
                   onTap: () {
-                    print('abc');
+                    setState(() {
+                      submitForm();
+                    });
                   },
                   outlineBtn: false,
-                  isLoading: false,
+                  isLoading: showLoading,
                 )
               ],
             ),
